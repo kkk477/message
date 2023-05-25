3da9e66e36ea2edc69241dafd7a75fc490d0a54e
HoWoong <dlghdnd@gmail.com>
2023-05-25 17:55:03 +0900

    섹션 3. 메시지, 국제화. 끝.

--- src/main/java/hello/itemservice/message/message/MessageItemController.java
+++ src/main/java/hello/itemservice/message/message/MessageItemController.java
@@ -1,4 +1,4 @@
-package hello.itemservice.web.message;
+package hello.itemservice.message.message;
 
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
